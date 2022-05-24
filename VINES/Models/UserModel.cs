cec2a4d8638492ad6b55fc7b810cc58cd23cb737
John Edwin Perez Cueto <johnedwin.cueto@benilde.edu.ph>
2022-05-24 22:46:20 +0800

    Add project files.

---
+++ VINES/Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VINES.Models
+{
+    public class UserModel
+    {
+        public void Test()
+        {
+            Debug.Write("Testing lang lmfao");
+        }
+    }
+}
