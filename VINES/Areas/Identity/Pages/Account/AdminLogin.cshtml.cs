7cc81930bd72e0d66ac5ff209a73cce2b3c71ede
Ramboyong, Chris Mitchell S <11438392@benilde.edu.ph>
2022-06-16 20:23:35 +0800

    Added AdminLogin and AdvertiserLogin view pages. Edited Login and Index.

---
+++ VINES/Areas/Identity/Pages/Account/AdminLogin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VINES.Areas.Identity.Pages.Account
+{
+    public class AdminLoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ VINES/Areas/Identity/Pages/Account/AdvertiserLogin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VINES.Areas.Identity.Pages
+{
+    public class AdvertiserLoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
