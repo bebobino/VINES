d0ff71a3e902090494a12ef05a898bfc0a277385
MiggyMessi <adrianmiguel.cortez@benilde.edu.ph>
2022-06-22 15:13:22 +0800

    merged rambo's to mine

--- VINES/Models/UserModel.cs
+++ VINES/Models/UserModel.cs
@@ -11,7 +11,7 @@ namespace VINES.Models
         public void Test()
         {
             //Test
-            Debug.Write("test push");
+            Debug.Write("Testing lang lmfao");
         }
     }
 }
