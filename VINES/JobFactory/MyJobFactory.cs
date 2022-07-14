6500ae86cd3327b9753f60be4c04c021c87745e8
John Edwin Perez Cueto <johnedwin.cueto@benilde.edu.ph>
2022-07-14 15:57:53 +0800

    eto na

---
+++ VINES/JobFactory/MyJobFactory.cs
@@ -0,0 +1,25 @@
+﻿using Quartz;
+using Quartz.Spi;
+using System;
+
+namespace VINES.JobFactory
+{
+    public class MyJobFactory : IJobFactory
+    {
+        private readonly IServiceProvider service;
+        public MyJobFactory(IServiceProvider service)
+        {
+            this.service = service;
+        }
+        public IJob NewJob(TriggerFiredBundle bundle, IScheduler scheduler)
+        {
+            var jobDetail = bundle.JobDetail;
+            return (IJob)service.GetService(jobDetail.JobType);
+        }
+
+        public void ReturnJob(IJob job)
+        {
+            
+        }
+    }
+}
---
+++ VINES/Scheduler/MyScheduler.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Hosting;
+using Quartz;
+using Quartz.Spi;
+using System.Threading;
+using System.Threading.Tasks;
+using VINES.Models;
+
+namespace VINES.Scheduler
+{
+    class MyScheduler : IHostedService
+    {
+        public IScheduler Scheduler { get; set; }
+        private readonly IJobFactory jobFactory;
+        private readonly JobModel jobMetadata;
+        private readonly ISchedulerFactory schedulerFactory;
+
+        public MyScheduler(ISchedulerFactory schedulerFactory, JobModel jobMetadata, IJobFactory jobFactory)
+        {
+            this.jobFactory = jobFactory;
+            this.jobMetadata = jobMetadata;
+            this.schedulerFactory = schedulerFactory;
+        }
+    
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            Scheduler = await schedulerFactory.GetScheduler();
+            Scheduler.JobFactory = jobFactory;
+            IJobDetail jobDetail = CreateJob(jobMetadata);
+            ITrigger trigger = CreateTrigger(jobMetadata);
+            await Scheduler.ScheduleJob(jobDetail, trigger, cancellationToken);
+            await Scheduler.Start(cancellationToken);
+        }
+
+        private IJobDetail CreateJob(JobModel jobMetadata)
+        {
+            return JobBuilder.Create(jobMetadata.JobType)
+                .WithIdentity(jobMetadata.JobID.ToString())
+                .WithDescription(jobMetadata.JobName)
+                .Build();
+        }
+
+        private ITrigger CreateTrigger(JobModel jobMetadata)
+        {
+            return TriggerBuilder.Create()
+                .WithIdentity(jobMetadata.JobID.ToString())
+                .WithCronSchedule(jobMetadata.CronExpression)
+                .WithDescription(jobMetadata.JobName)
+                .Build();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
