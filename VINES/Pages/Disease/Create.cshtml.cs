064b7044f20da86276d37b7a64c29369eb1c57ee
MiggyMessi <adrianmiguel.cortez@benilde.edu.ph>
2022-09-30 11:37:35 +0800

    Fixed admin's side Create Frontend/backend validations.

--- VINES/Pages/Disease/Create.cshtml.cs
+++ VINES/Pages/Disease/Create.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using VINES.Models;
@@ -19,24 +20,59 @@ namespace VINES.Pages.Disease
             diseases = _Db.Diseases.ToList();
         }
         [BindProperty]
-        public Diseases Diseases{ get; set; }
-        public void OnGet()
-        {
-        }
-        public async Task<IActionResult> OnPost(int typeID, string name, string notes )
-        {
-            var diseases = new Diseases
-            {
-                diseaseID = typeID,
-                diseaseName = name,
-                notes = notes,
-                dateAdded = DateTime.Now,
-                dateModified = DateTime.Now
-            };
+        public InputModel Input { get; set; }
+        public string ReturnUrl { get; set; }
 
-            _Db.Diseases.Add(diseases);
-            await _Db.SaveChangesAsync();
-            return RedirectToPage("Index");
+        public class InputModel
+        {
+            [Required]
+            [Display(Name = "Disease Name")]
+            [RegularExpression(@"^[a-zA-Z0-9 -#]*$", ErrorMessage = "The name contains invalid characters.")]
+            public string diseaseName { get; set; }
+
+            [Display(Name = "Notes")]
+            [RegularExpression(@"^[a-zA-Z0-9 -#]*$", ErrorMessage = "The name contains invalid characters.")]
+            public string notes { get; set; }
+            [Required]
+            public DateTime dateAdded { get; set; }
+            [Required]
+            public DateTime dateModified { get; set; }
+        }
+
+        public async Task OnGetAsync(string returnUrl)
+        {
+            ReturnUrl = returnUrl;
+        }
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            ReturnUrl ??= Url.Content("~/");
+
+            if (ModelState.IsValid)
+            {
+                var disease = _Db.diseases.Where(d => d.diseaseName == Input.diseaseName).FirstOrDefault();
+                if (disease != null)
+                {
+                    ModelState.AddModelError("Error", "Already in Database.");
+                }
+                else
+                {
+                    ModelState.AddModelError("Success", "Disease has been successfully added.");
+                    disease = new Diseases
+                    {
+                        diseaseName = Input.diseaseName,
+                        notes = Input.notes,
+                        dateAdded = DateTime.Now,
+                        dateModified = DateTime.Now
+                    };
+
+
+                    _Db.Diseases.Add(disease);
+                    await _Db.SaveChangesAsync();
+                    return RedirectToPage("Index");
+                }
+            }
+
+            return Page();
         }
     }
 }
--- VINES/Pages/Institution/Create.cshtml.cs
+++ VINES/Pages/Institution/Create.cshtml.cs
@@ -2,8 +2,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using VINES.Models;
 
 namespace VINES.Pages.Institution
@@ -19,26 +21,71 @@ namespace VINES.Pages.Institution
             institutionTypes = _Db.InstitutionTypes.ToList();
         }
         [BindProperty]
-        public Institutions institutions { get; set; }
-        public void OnGet()
-        {
-        }
-        public async Task<IActionResult> OnPost(int typeID, string name, decimal Long, decimal lat, string notes )
-        {
-            var institutions = new Institutions
-            {
-                institutionTypeID = typeID,
-                institutionName = name,
-                Long = Long,
-                lat = lat,
-                notes = notes,
-                dateAdded = DateTime.Now,
-                lastModified = DateTime.Now
-            };
+        public InputModel Input { get; set; }
+        public string ReturnUrl { get; set; }
 
-            _Db.Institutions.Add(institutions);
-            await _Db.SaveChangesAsync();
-            return RedirectToPage("Index");
+        public class InputModel
+        {
+            [Required]
+            public int institutionTypeID { get; set; }
+            [Required]
+            [Display(Name = "Institution Name")]
+            [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$",
+            ErrorMessage = "Characters are not allowed.")]
+            public string institutionName { get; set; }
+            [Required]
+            [Display(Name = "Longitude")]
+            public decimal Long { get; set; }
+            [Required]
+            [Display(Name = "Latitude")]
+            public decimal lat { get; set; }
+
+            [Display(Name = "Notes")]
+            [RegularExpression(@"^[a-zA-Z0-9 -#]*$", ErrorMessage = "The name contains invalid characters.")]
+            public string notes { get; set; }
+            [Required]
+            public DateTime dateAdded { get; set; }
+            [Required]
+            public DateTime lastModified { get; set; }
+        };
+
+        public async Task OnGetAsync(string returnUrl = null)
+        {
+            ReturnUrl = returnUrl;
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+
+            if (ModelState.IsValid)
+            {
+                var insti = _Db.Institutions.Where(f => f.institutionName == Input.institutionName).FirstOrDefault();
+                if (insti != null)
+                {
+                    ModelState.AddModelError("Error", "Already in Database");
+                }
+                else
+                {
+                    ModelState.AddModelError("Success", "Institution Has Been Added");
+                    insti = new Institutions
+                    {
+                        institutionTypeID = Input.institutionTypeID,
+                        institutionName = Input.institutionName,
+                        Long = Input.Long,
+                        lat = Input.lat,
+                        notes = Input.notes,
+                        dateAdded = DateTime.Now,
+                        lastModified = DateTime.Now,
+                    };
+
+                    _Db.Institutions.Add(insti);
+                    await _Db.SaveChangesAsync();
+                    return RedirectToPage("/Institution/Index");
+                }
+            }
+
+            return Page();
         }
     }
 }
