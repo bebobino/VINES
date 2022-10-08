3143056a50cfd5df8f12a3154b19a8befc34ed30
John Edwin Perez Cueto <johnedwin.cueto@benilde.edu.ph>
2022-10-08 20:15:51 +0800

    fix nyo yung routing at yung itsure pages/patient/preference

---
+++ VINES/Pages/Patient/Preference.cshtml.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VINES.Models;
+
+namespace VINES.Pages.Patient
+{
+    public class Preference : PageModel
+    {
+        private readonly DatabaseContext _Db;
+        public List<CommunityPostCategoriesModel> categories { get; set; }
+       
+        public List<InstitutionVaccines> inst { get; set; }
+        public List<Vaccines> vac { get; set; }
+        public List<Institutions> i { get; set; }
+        public List<InstitutionTypes> ii { get; set; }
+        public Preference(DatabaseContext Db)
+        {
+            _Db = Db;
+        }
+        public void OnGet(int vax, decimal budget)
+        {
+            inst = _Db.InstitutionVaccines.Where(v => v.vaccineID == vax && v.price <= budget).ToList();
+            i = _Db.Institutions.ToList();
+            ii = _Db.InstitutionTypes.ToList();
+            vac = _Db.vaccines.ToList();
+        }
+
+        
+    }
+}
