bcc3f0b7d4d115521c518a4fc28e9abbb8a603d2
John Edwin Perez Cueto <johnedwin.cueto@benilde.edu.ph>
2022-10-08 20:41:31 +0800

    lol

--- VINES/Pages/Patient/Preference.cshtml.cs
+++ VINES/Pages/Patient/Preference.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System;
@@ -8,6 +9,7 @@ using VINES.Models;
 
 namespace VINES.Pages.Patient
 {
+    [Authorize("PatientOnly")]
     public class Preference : PageModel
     {
         private readonly DatabaseContext _Db;
